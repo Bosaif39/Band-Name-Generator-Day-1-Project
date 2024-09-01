79bd3c285a6092e896904ee5257eafb8b516d716
Abdullah Saif <bosaif39tm@gmail.com>
2024-09-01 18:42:09 +0400

    Add files via upload

---
+++ Day 1 Project Band Name Generator.py
@@ -0,0 +1,5 @@
+print("Welcome to Band Name Generator.")
+city=input("What's name of the ciry you grew up in?\n")
+pet=input("What's your pet's name?\n")
+print("Your band name could be "+city+" "+pet+"\n\n")
+                                                
